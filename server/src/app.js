6842f60633f32213ab1b9eb1c3aa7bdb15ebf7ca
dan.zhang <dan.zhang@developersinstitute.co.nz>
2022-08-19 11:23:02 +1200

    create view a single restanrant route and test pass

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const RestanrantModel = require("./models/RestaurantModel");
 const formatRestanrant = require("./formatRestanrant");
 
 app.use(cors());
 app.use(express.json());
 
-// get all restanrants
+// User Story #1 - View all restaurants
 app.get("/restanrants", async (req, res) => {
   const restanrants = await RestanrantModel.find({});
-  console.log(restanrants);
   return res.status(200).send(restanrants.map(formatRestanrant));
 });
+// ser Story #2 - View a single restaurant
+app.get("/restanrants/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ error: "id is unavild" });
+  }
 
+  const oneRestanrant = await RestanrantModel.findById(id);
+  if (oneRestanrant === null) {
+    return res.status(404).send("not found");
+  }
+  return res.status(200).send(formatRestanrant(oneRestanrant));
+});
+// User Story #3 - Book a reservation
+
+// User Story #4 - View all my reservations
+
+// User Story #5 - View a single reservation
 module.exports = app;
--- server/src/app.test.js
+++ server/src/app.test.js
@@ -2,8 +2,9 @@ const request = require("supertest");
 const app = require("./app");
 const restanrantsForTest = require("./testData/restanrantsForTest.json");
 
-describe("app", () => {
-  it("GET /restanrants should return a list of restanrants", async () => {
+// User Story #1 - View all restaurants
+describe("1 - View all restaurants", () => {
+  it("GET /restanrants should return a list of restanrants and 200 status", async () => {
     const expectedStatus = 200;
     const expectedBody = restanrantsForTest;
 
@@ -12,7 +13,36 @@ describe("app", () => {
       .expect(expectedStatus)
       .expect((res) => {
         expect(res.body).toEqual(expectedBody);
-        console.log(res.body);
       });
   });
 });
+// ser Story #2 - View a single restaurant:happy and unhappy path
+describe("2 - View a single restaurant", () => {
+  it("GET /restanrants/:id should return a single restanrant", async () => {
+    const expectedStatus = 200;
+    const expectedBody = restanrantsForTest[0];
+    await request(app)
+      .get("/restanrants/616005cae3c8e880c13dc0b9")
+      .expect(expectedStatus)
+      .expect((res) => {
+        expect(res.body).toEqual(expectedBody);
+      });
+  });
+  it("GET /restanrants/:id should return 404 when request is a not exist id", async () => {
+    const expectedStatus = 404;
+    await request(app)
+      .get("/restanrants/616005cae3c8e880c13dc0b0")
+      .expect(expectedStatus);
+  });
+  it("GET /restanrants/:id should return 400 when request is an invalid id", async () => {
+    const expectedStatus = 400;
+    await request(app)
+      .get("/restanrants/616005cae3c8e880c13")
+      .expect(expectedStatus);
+  });
+});
+// User Story #3 - Book a reservation
+
+// User Story #4 - View all my reservations
+
+// User Story #5 - View a single reservation
