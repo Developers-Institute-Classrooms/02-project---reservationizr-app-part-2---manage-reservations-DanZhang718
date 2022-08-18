b523d940c165ded623482544dc2a1e37fa1e485d
dan.zhang <dan.zhang@developersinstitute.co.nz>
2022-08-18 15:57:42 +1200

    create restanrant mongoose model

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,1 +1,11 @@
 // FIXME: Add a Mongoose model here
+const mongoose = require("mongoose");
+const { Scheam } = mongoose;
+
+const restanrantSchema = new Scheam({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+});
+const Restanrant = mongoose.model("Restanrant", restanrantSchema);
+module.exports = Restanrant;
