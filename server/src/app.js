5b47afddd232d73266095ad5f870ab8e0879ab85
dan.zhang <dan.zhang@developersinstitute.co.nz>
2022-08-22 12:14:56 +1200

    add display one restaurant in server side and pass test

--- server/src/app.js
+++ server/src/app.js
@@ -2,29 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-const RestanrantModel = require("./models/RestaurantModel");
-const formatRestanrant = require("./formatRestanrant");
+const RestaurantModel = require("./models/RestaurantModel");
+const formatRestaurant = require("./formatRestanrant");
 
 app.use(cors());
 app.use(express.json());
 
 // User Story #1 - View all restaurants
-app.get("/restanrants", async (req, res) => {
-  const restanrants = await RestanrantModel.find({});
-  return res.status(200).send(restanrants.map(formatRestanrant));
+app.get("/restaurants", async (req, res) => {
+  console.log(req);
+  const restaurants = await RestaurantModel.find({});
+  return res.status(200).send(restaurants.map(formatRestaurant));
 });
 // ser Story #2 - View a single restaurant
-app.get("/restanrants/:id", async (req, res) => {
+app.get("/restaurants/:id", async (req, res) => {
+  console.log(req);
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({ error: "id is unavild" });
   }
-
-  const oneRestanrant = await RestanrantModel.findById(id);
-  if (oneRestanrant === null) {
-    return res.status(404).send("not found");
+  const oneRestaurant = await RestaurantModel.findById(id);
+  if (oneRestaurant === null) {
+    return res.status(404).send({ error: "not found" });
   }
-  return res.status(200).send(formatRestanrant(oneRestanrant));
+  return res.status(200).send(formatRestaurant(oneRestaurant));
 });
 // User Story #3 - Book a reservation
 
