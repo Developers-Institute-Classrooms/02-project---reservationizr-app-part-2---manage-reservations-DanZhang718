4f98cba6d99e1bbde1621bab29ab80c2733cb60e
dan.zhang <dan.zhang@developersinstitute.co.nz>
2022-08-23 11:59:08 +1200

    recreate format restaurant

--- server/src/formatRestanrant.js
+++
@@ -1,9 +0,0 @@
-const formatRestanrant = (restanrant) => {
-  return {
-    id: restanrant._id,
-    name: restanrant.name,
-    description: restanrant.description,
-    image: restanrant.image,
-  };
-};
-module.exports = formatRestanrant;
