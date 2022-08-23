abc21017c89eeb14c6c7d829cb89e5258fd496e5
dan.zhang <dan.zhang@developersinstitute.co.nz>
2022-08-23 13:30:36 +1200

    install jwt package

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 // import { useAuth0 } from "@auth0/auth0-react";
-// import ProtectedRoute from "./auth/ProtectedRoute";
+import ProtectedRoute from "./auth/ProtectedRoute";
 import Header from "./components/Header";
 // import Reservation from "./components/Reservation";
 import ReservationList from "./components/ReservationList";
@@ -19,7 +19,9 @@ const App = () => {
       <Header />
       <div className="container">
         <Routes>
-          <Route path="/restaurants/:id" element={<Restaurant />} />
+          <Route path="/restaurants/:id" element={<ProtectedRoute />}>
+            <Route path="/restaurants/:id" element={<Restaurant />} />
+          </Route>
           {/* <Route path="/reservations/:id" element={<Reservation />} /> */}
           <Route path="/reservations" element={<ReservationList />} />
           <Route path="/" element={<RestaurantList />} />
